99e4d4d3db419d3caa70d16fe819dc450ff164ae
Kristen Bae <knbae@outlook.com>
2022-02-01 22:03:27 -0800

    no message

---
+++ snuzeapp/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const alabaster = const Color.fromRGBO(220, 224, 217, 100);
+const darkPurple = const Color.fromRGBO(49, 8, 31, 100);
+const persianPlum = const Color.fromRGBO(107, 15, 26, 100);
+const davysGrey = const Color.fromRGBO(89, 89, 89, 100);
+
+const kSnuzeBrown900 = const Color.fromRGBO(71, 106, 111, 50);
+
+const kSnuzeErrorRed = const Color(0xFFC5032B);
+
+const xanadu = const Color.fromRGBO(128, 143, 133, 100);
+const kSnuzeBackgroundWhite = Colors.white;
